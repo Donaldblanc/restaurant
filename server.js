bacbd0a61afe2ac585af83d4444b3075e1a98441
joglatt <34425753+joglatt@users.noreply.github.com>
2018-05-12 11:02:12 -0400

    added html to tables

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ app.use(bodyParser.json());
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
-  
-
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });  
+  app.get("/api/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
 
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
